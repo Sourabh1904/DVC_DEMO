990ee98619c58264343d1e1d72eeadc01580ae88
Sourabh Sanjay Sawant <sawantsourabh1904@gmail.com>
2025-09-25 22:03:50 +0530

    added pytest example

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine project package", 
+    author="Sourabh", 
+    packages=find_packages(),
+    license="MIT"
+)
